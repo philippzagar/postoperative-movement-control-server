7e3be9d33a2063f266518369780590fd66ad72ee
Philipp1999 <philipp.zagar@gmail.com>
2017-09-29 21:37:45 +0200

    Implementing Mongoose and playing around with it

--- app.js
+++ app.js
@@ -22,13 +22,61 @@ argv.l ? logArgument = true : logArgument = false;
 
 let log = new Log(logArgument);
 
-const {MongoClient} = require('mongodb');
+//const {MongoClient, ObjectID} = require('mongodb');
+let mongoose = require('mongoose');
 
 const user = "MyAppUser";
 const pw = "12345";
 const authMechanism = "SCRAM-SHA-1";
 const authSource = "MyApp";
 
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://${user}:${pw}@localhost:27017/MyApp?authMechanism=${authMechanism}`,
+    {
+        useMongoClient: true,
+    }
+);
+
+let Member = mongoose.model('Member', {
+    first_name: {
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true
+    },
+    last_name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    under_18: {
+        type: Boolean,
+        required: true
+    },
+    birth_year: {
+        type: Number,
+        minlength: 4,
+        default: null
+    }
+});
+
+let newMember = new Member({
+    first_name: "Philipp",
+    last_name: "Zagar",
+    under_18: false,
+    birth_year: 1999
+});
+
+newMember.save().then((doc) => {
+    log.Console("Saved");
+    log.ConsoleJSON(doc);
+}, (err) => {
+    log.Console("Unable to save!");
+    log.ConsoleJSON(err);
+});
+
+/*
 // Connection URL
 const url = `mongodb://${user}:${pw}@localhost:27017/MyApp?authMechanism=${authMechanism}`;
 // Use connect method to connect to the Server
@@ -40,7 +88,7 @@ MongoClient.connect(url, function(err, db) {
 
     log.Console("Connected correctly to server");
 
-    /*
+
     db.collection("Test").insertOne({
         name: "Philipp",
         last_name: "Zagar"
@@ -51,7 +99,7 @@ MongoClient.connect(url, function(err, db) {
         log.ConsoleJSON(result.ops[0]);
         log.Console(result.ops[0]._id.getTimestamp());
     });
-    */
+
 
     db.collection("Test").find({last_name: "Zagar"}).toArray().then((docs) => {
         log.ConsoleJSON(docs);
@@ -68,12 +116,9 @@ MongoClient.connect(url, function(err, db) {
 
     db.close();
 });
-
+*/
 //sql.insertQuery(`INSERT INTO log (time, data) VALUES ('${date.getDateTime()}', 'Test for the sql function 2')`);
 //log.Console("test", logArgument);
-
-//log.logDB("This is a Test", logArgument);
-
 /*app.get('/', (req, res) => {
     res.send("Hallo");
     console.log("Inside get funciton");
